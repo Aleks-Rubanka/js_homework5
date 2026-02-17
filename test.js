ba8a9451f82c778f14244dcb0f63637ecdf7948b
Aleks-Rubanka <logdog92938@gmail.com>
2026-02-17 11:49:07 +0200

    Update test.js

--- test.js
+++ test.js
@@ -2,62 +2,54 @@
 var service = {
   services: {
     "стрижка": "60 грн",
-    "гоління": "80 грн",
-    "Миття голови": "100 грн"
+    "гоління": "80.5 грн",
+    "Миття голови": "100.25 грн"
   }
 };
 
 // 🔹 Додаємо нову послугу
-service.services["Розбити скло"] = "200 грн";
-
+service.services["Розбити скло"] = "200.75 грн";
 
 // 🔹 Метод загальної вартості
 service.price = function () {
   var total = 0;
 
   for (var key in this.services) {
-    var number = parseInt(this.services[key]);
+    var number = parseFloat(this.services[key]); // замість parseInt
     total += number;
   }
 
   return total + " грн";
 };
 
-
 // 🔹 Метод мінімальної ціни
 service.minPrice = function () {
   var min = Infinity;
 
   for (var key in this.services) {
-    var number = parseInt(this.services[key]);
-
-    if (number < min) {
-      min = number;
-    }
+    var number = parseFloat(this.services[key]); // замість parseInt
+    if (number < min) min = number;
   }
 
   return min + " грн";
 };
-
 
 // 🔹 Метод максимальної ціни
 service.maxPrice = function () {
   var max = 0;
 
   for (var key in this.services) {
-    var number = parseInt(this.services[key]);
-
-    if (number > max) {
-      max = number;
-    }
+    var number = parseFloat(this.services[key]); // замість parseInt
+    if (number > max) max = number;
   }
 
   return max + " грн";
 };
 
-
-// 🔹 Валідація (перевірка роботи)
+// 🔹 Перевірка
 console.log("Послуги:", service.services);
 console.log("Загальна сума:", service.price());
 console.log("Мінімальна ціна:", service.minPrice());
 console.log("Максимальна ціна:", service.maxPrice());
+
+
